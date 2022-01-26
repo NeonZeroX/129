e4635759e5dfccefc9e33daa4b3b35a4ce380ba4
NeonZeroX <80101013+NeonZeroX@users.noreply.github.com>
2022-01-26 16:35:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
     <meta name="generator" content="Nicepage 4.3.3, nicepage.com">
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
-    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700|Playfair+Display:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Atomic+Age:400">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Oswald:200,300,400,500,600,700|Playfair+Display:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Atomic+Age:400">
     
     
     
@@ -31,18 +31,19 @@
     <meta property="og:title" content="about">
     <meta property="og:type" content="website">
   </head>
-  <body class="u-body"><header class="u-clearfix u-header u-header" id="sec-2e77"><div class="u-align-left u-clearfix u-sheet u-sheet-1"></div></header>
+  <body class="u-body u-palette-1-base"><header class="u-clearfix u-header u-header" id="sec-2e77"><div class="u-align-left u-clearfix u-sheet u-sheet-1"></div></header>
     <section class="u-align-center u-clearfix u-image u-section-1" id="carousel_b035" data-image-width="1280" data-image-height="853">
       <div class="u-black u-expanded-width u-shape u-shape-rectangle u-shape-1"></div>
       <div alt="" class="u-image u-image-circle u-image-1" data-image-width="1440" data-image-height="1440"></div>
       <h2 class="u-text u-text-white u-text-1"><b style=""></b>
-        <a href="http://mmtkmm.blogspot.com/2013/01/my-profile.html" class="u-border-none u-btn u-button-style u-custom-font u-font-oswald u-hover-palette-1-dark-1 u-none u-text-grey-25 u-btn-1"><b>MY LIFE STORY</b>
-        </a>
       </h2>
-      <p class="u-text u-text-white u-text-2">Jirawat Chartee<br>
-        <span style="font-size: 1.25rem;">63118343</span>
+      <h1 class="u-text u-text-palette-5-base u-text-2"> MY LIFE STORY</h1>
+      <p class="u-custom-font u-font-montserrat u-text u-text-white u-text-3">
+        <span style="font-size: 2.25rem;">
+          <span style="font-weight: 700;">Jirawat Chartee</span>
+          <br>
+        </span>63118343
       </p>
-      <a href="https://nicepage.dev" class="u-active-none u-border-2 u-border-white u-btn u-btn-rectangle u-button-style u-hover-none u-none u-btn-2">read more</a>
     </section>
     <section class="u-align-left u-clearfix u-image u-section-2" id="carousel_f8d3" data-image-width="1280" data-image-height="640">
       <div class="u-clearfix u-sheet u-sheet-1">
@@ -504,9 +505,8 @@
                 </div>
               </div>
               <div class="u-container-style u-layout-cell u-right-cell u-size-38 u-layout-cell-2">
-                <div class="u-container-layout u-valign-bottom u-container-layout-2">
+                <div class="u-container-layout u-container-layout-2">
                   <p class="u-custom-font u-font-courier-new u-text u-text-body-alt-color u-text-3"> I once had the opportunity to study exchange in Vietnam and this is a good memory of my first overseas trip. There was both fun and strange at that time. but i'm not good at language I am very excited when I meet Vietnamese people. But anyway, I want to go to Vietnam one more time.</p>
-                  <a href="https://nicepage.com/k/teacher-website-templates" class="u-active-white u-border-none u-btn u-btn-round u-button-style u-hover-white u-palette-3-base u-radius-50 u-text-active-palette-1-base u-text-hover-palette-1-base u-text-palette-1-base u-btn-1">read more</a>
                 </div>
               </div>
             </div>
@@ -564,11 +564,10 @@
                 <div class="u-container-layout u-container-layout-1"></div>
               </div>
               <div class="u-black u-container-style u-layout-cell u-size-45-lg u-size-45-md u-size-45-xl u-size-60-sm u-size-60-xs u-layout-cell-2">
-                <div class="u-container-layout u-valign-bottom u-container-layout-2">
+                <div class="u-container-layout u-container-layout-2">
                   <h1 class="u-align-center u-custom-font u-text u-text-1">My dream career</h1>
                   <p class="u-align-left u-custom-font u-font-courier-new u-text u-text-2"> Once I had the opportunity to test myself by competing in an&nbsp;<br>e-sport of Realm of Valor, also known as Rov, competing in the 4th season of king of gamer. alone And I think I'm doing well. able to reach the final But I had to miss the opportunity. Because my family didn't have the support to compete offline, I missed the opportunity to be an e-sport athlete. But at least I gained experience from that race. And that's one of my dreams that I'd like to achieve one day.
                   </p>
-                  <a href="https://nicepage.com/k/teacher-website-templates" class="u-active-black u-border-none u-btn u-button-style u-hover-black u-palette-3-base u-btn-1">learn more</a>
                 </div>
               </div>
             </div>
@@ -604,11 +603,10 @@
         <h3 class="u-align-left u-custom-font u-font-oswald u-text u-text-2">t<span class="u-text-palette-3-base" style="font-weight: 700;"> my life concept</span>
         </h3>
         <p class="u-align-left u-text u-text-body-alt-color u-text-3"> Sometimes mistakes can be the best memories of your life.</p>
-        <a href="https://nicepage.com/c/pricing-html-templates" class="u-border-2 u-border-active-white u-border-hover-white u-border-white u-btn u-button-style u-hover-white u-none u-text-active-palette-2-base u-text-body-alt-color u-text-hover-palette-2-base u-btn-1">learn more</a>
       </div>
     </section>
     <section class="u-align-center u-clearfix u-grey-5 u-section-7" id="sec-b105">
-      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+      <div class="u-clearfix u-sheet u-sheet-1">
         <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
           <div class="u-gutter-0 u-layout">
             <div class="u-layout-row">
@@ -618,28 +616,40 @@
                 </div>
               </div>
               <div class="u-align-center u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
-                <div class="u-container-layout u-valign-middle u-container-layout-2">
+                <div class="u-container-layout u-container-layout-2">
                   <div class="u-social-icons u-spacing-10 u-social-icons-1">
-                    <a class="u-social-url" title="facebook" target="_blank" href="https://facebook.com/name"><span class="u-icon u-social-facebook u-social-icon u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112.196 112.196" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-194c"></use></svg><svg class="u-svg-content" viewBox="0 0 112.196 112.196" x="0px" y="0px" id="svg-194c" style="enable-background:new 0 0 112.196 112.196;"><g><circle style="fill:currentColor;" cx="56.098" cy="56.098" r="56.098"></circle><path style="fill:#FFFFFF;" d="M70.201,58.294h-10.01v36.672H45.025V58.294h-7.213V45.406h7.213v-8.34   c0-5.964,2.833-15.303,15.301-15.303L71.56,21.81v12.51h-8.151c-1.337,0-3.217,0.668-3.217,3.513v7.585h11.334L70.201,58.294z"></path>
+                    <a class="u-social-url" title="facebook" target="_blank" href="https://www.facebook.com/profile.php?id=100009082605167"><span class="u-icon u-social-facebook u-social-icon u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112.196 112.196" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-194c"></use></svg><svg class="u-svg-content" viewBox="0 0 112.196 112.196" x="0px" y="0px" id="svg-194c" style="enable-background:new 0 0 112.196 112.196;"><g><circle style="fill:currentColor;" cx="56.098" cy="56.098" r="56.098"></circle><path style="fill:#FFFFFF;" d="M70.201,58.294h-10.01v36.672H45.025V58.294h-7.213V45.406h7.213v-8.34   c0-5.964,2.833-15.303,15.301-15.303L71.56,21.81v12.51h-8.151c-1.337,0-3.217,0.668-3.217,3.513v7.585h11.334L70.201,58.294z"></path>
 </g></svg></span>
                     </a>
-                    <a class="u-social-url" title="twitter" target="_blank" href="https://twitter.com/name"><span class="u-icon u-social-icon u-social-twitter u-icon-2"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112.197 112.197" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-037f"></use></svg><svg class="u-svg-content" viewBox="0 0 112.197 112.197" x="0px" y="0px" id="svg-037f" style="enable-background:new 0 0 112.197 112.197;"><g><circle style="fill:#55ACEE;" cx="56.099" cy="56.098" r="56.098"></circle><g><path style="fill:#F1F2F2;" d="M90.461,40.316c-2.404,1.066-4.99,1.787-7.702,2.109c2.769-1.659,4.894-4.284,5.897-7.417    c-2.591,1.537-5.462,2.652-8.515,3.253c-2.446-2.605-5.931-4.233-9.79-4.233c-7.404,0-13.409,6.005-13.409,13.409    c0,1.051,0.119,2.074,0.349,3.056c-11.144-0.559-21.025-5.897-27.639-14.012c-1.154,1.98-1.816,4.285-1.816,6.742    c0,4.651,2.369,8.757,5.965,11.161c-2.197-0.069-4.266-0.672-6.073-1.679c-0.001,0.057-0.001,0.114-0.001,0.17    c0,6.497,4.624,11.916,10.757,13.147c-1.124,0.308-2.311,0.471-3.532,0.471c-0.866,0-1.705-0.083-2.523-0.239    c1.706,5.326,6.657,9.203,12.526,9.312c-4.59,3.597-10.371,5.74-16.655,5.74c-1.08,0-2.15-0.063-3.197-0.188    c5.931,3.806,12.981,6.025,20.553,6.025c24.664,0,38.152-20.432,38.152-38.153c0-0.581-0.013-1.16-0.039-1.734    C86.391,45.366,88.664,43.005,90.461,40.316L90.461,40.316z"></path>
-</g>
-</g></svg></span>
+                    <a class="u-social-url" target="_blank" data-type="Phone" title="Phone" href="0994093232"><span class="u-icon u-social-icon u-social-phone u-icon-2"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-3cb6"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-3cb6"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M82.7,66c-3.9,0-7.7-0.6-11.3-1.8c-1.8-0.6-4,0-5,1.1l-7.1,5.4c-8.3-4.4-13.4-9.5-17.7-17.7l5.2-7
+	c1.4-1.4,1.8-3.3,1.3-5.2c-1.2-3.6-1.8-7.5-1.8-11.3c0-2.8-2.3-5.1-5.1-5.1H29.5c-2.8,0-5.1,2.3-5.1,5.1c0,32.2,26.2,58.4,58.4,58.4
+	c2.8,0,5.1-2.3,5.1-5.1V71.1C87.8,68.3,85.6,66,82.7,66z"></path></svg></span>
                     </a>
-                    <a class="u-social-url" target="_blank" data-type="Instagram" title="Instagram" href=""><span class="u-icon u-social-icon u-social-instagram u-icon-3"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-7eb4"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-7eb4"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M55.9,38.2c-9.9,0-17.9,8-17.9,17.9C38,66,46,74,55.9,74c9.9,0,17.9-8,17.9-17.9C73.8,46.2,65.8,38.2,55.9,38.2
-            z M55.9,66.4c-5.7,0-10.3-4.6-10.3-10.3c-0.1-5.7,4.6-10.3,10.3-10.3c5.7,0,10.3,4.6,10.3,10.3C66.2,61.8,61.6,66.4,55.9,66.4z"></path><path fill="#FFFFFF" d="M74.3,33.5c-2.3,0-4.2,1.9-4.2,4.2s1.9,4.2,4.2,4.2s4.2-1.9,4.2-4.2S76.6,33.5,74.3,33.5z"></path><path fill="#FFFFFF" d="M73.1,21.3H38.6c-9.7,0-17.5,7.9-17.5,17.5v34.5c0,9.7,7.9,17.6,17.5,17.6h34.5c9.7,0,17.5-7.9,17.5-17.5V38.8
-            C90.6,29.1,82.7,21.3,73.1,21.3z M83,73.3c0,5.5-4.5,9.9-9.9,9.9H38.6c-5.5,0-9.9-4.5-9.9-9.9V38.8c0-5.5,4.5-9.9,9.9-9.9h34.5
-            c5.5,0,9.9,4.5,9.9,9.9V73.3z"></path></svg></span>
+                    <a class="u-social-url" target="_blank" data-type="Spotify" title="Spotify" href="https://open.spotify.com/user/31h7yp3zkgnj2klvhe6tya3tjc5a?si=2cf5835cf0f743da"><span class="u-icon u-social-icon u-social-spotify u-icon-3"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-ae67"></use></svg><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-ae67"><circle fill="#00D95F" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFE" d="M94.5,41.9c-1.9-1.1-3.8-2.1-5.8-3c-2-0.9-4-1.7-6.1-2.4c-5.1-1.7-10.2-3-15.4-3.9
+	c-5.2-0.9-10.5-1.3-15.8-1.5c-2.2-0.1-4.3-0.1-6.5,0c-2.2,0.1-4.3,0.2-6.5,0.4c-2.1,0.2-4.1,0.4-6.1,0.7c-2,0.3-4,0.7-6,1.1
+	c-0.8,0.2-1.6,0.4-2.4,0.6c-0.8,0.2-1.6,0.5-2.4,0.7c-1.3,0.4-2.4,1.3-3.1,2.3c-0.7,1.1-1.1,2.4-1.1,3.7c0,1.4,0.5,2.7,1.3,3.7
+	c0.8,1,1.9,1.8,3.2,2.2c0.6,0.2,1.2,0.3,1.8,0.2c0.6,0,1.2-0.1,1.8-0.2l0.6-0.2c1.4-0.4,2.8-0.7,4.2-1c2.2-0.5,4.3-0.8,6.5-1.1
+	c2.2-0.3,4.4-0.4,6.6-0.6c1.9-0.1,3.9-0.1,5.8-0.1c1.9,0,3.8,0.1,5.8,0.2c1.9,0.1,3.7,0.3,5.6,0.5c1.8,0.2,3.6,0.5,5.5,0.8
+	c2.6,0.5,5.2,1,7.8,1.7c2.5,0.7,5.1,1.5,7.5,2.5c1.3,0.5,2.5,1,3.6,1.6c1.2,0.6,2.4,1.2,3.5,1.9c1,0.6,2.1,0.8,3.2,0.9
+	c1.1,0,2.2-0.3,3.2-0.8c0.9-0.6,1.8-1.3,2.3-2.3c0.5-0.9,0.8-2,0.8-3.1l0-0.2c0,0,0-0.2,0-0.3c0-0.3-0.1-0.6-0.1-0.9
+	c-0.2-0.9-0.6-1.7-1.1-2.4C95.9,42.9,95.2,42.3,94.5,41.9z"></path><path fill="#FFFFFE" d="M73,54c-4.7-1.5-9.5-2.6-14.4-3.3c-4.9-0.7-9.8-0.9-14.7-0.7c-2.3,0.1-4.6,0.3-6.9,0.5
+	c-2.3,0.3-4.5,0.7-6.8,1.2c-0.9,0.2-1.9,0.4-2.8,0.7c-0.9,0.2-1.8,0.5-2.7,0.8c-1.1,0.3-2,1-2.7,1.9c-0.7,0.9-1,2-1.1,3.1
+	c0,1.1,0.3,2.2,0.9,3.1c0.6,0.9,1.5,1.6,2.5,2.1c0.6,0.3,1.3,0.4,2,0.4c0.6,0,1.3-0.1,1.9-0.3c3-0.9,6-1.5,9-1.9
+	c3.2-0.5,6.5-0.7,9.7-0.7c2.5,0,4.9,0.1,7.3,0.3c2.4,0.2,4.7,0.5,7,1c3,0.6,5.9,1.3,8.8,2.2c2.9,0.9,5.7,2.1,8.4,3.5
+	c0.9,0.4,1.7,0.9,2.6,1.4l0.9,0.5c1.3,0.7,2.8,0.9,4.1,0.6c1.3-0.3,2.5-1.2,3.3-2.4c0.7-1.2,1-2.7,0.6-4.2c-0.3-1.3-1.1-2.5-2.4-3.3
+	l0,0c-0.2-0.1-0.5-0.3-0.6-0.4c-2.2-1.3-4.4-2.4-6.7-3.5C77.8,55.6,75.4,54.7,73,54z"></path><path fill="#FFFFFE" d="M58.7,68.6c-3.6-0.5-7.2-0.8-10.9-0.9h-0.1c-0.8,0-1.5,0.1-2.3,0.1c-1.7,0.1-3.5,0.2-5.1,0.3
+	c-2.2,0.2-4.4,0.5-6.6,0.8c-2.2,0.4-4.4,0.8-6.5,1.3c-0.2,0-0.3,0.1-0.6,0.1h0c-0.2,0.1-0.4,0.1-0.6,0.2c-1,0.4-1.8,1.1-2.3,2.1
+	c-0.5,0.9-0.7,2-0.5,3.1c0.2,1.1,0.8,2,1.6,2.6c0.8,0.6,1.8,1,2.9,1c0.3,0,0.7,0,1-0.1c0.3,0,0.6-0.1,0.9-0.2c2.6-0.6,5.2-1,7.8-1.4
+	c2.6-0.4,5.2-0.6,7.9-0.7c2-0.1,4.1-0.1,6.1,0c2,0.1,4,0.3,6,0.6c3.3,0.5,6.4,1.3,9.5,2.3c3.1,1.1,6,2.4,8.8,4.1l0,0l0.1,0.1
+	c0.3,0.2,0.6,0.3,0.9,0.5c1.1,0.5,2.2,0.5,3.2,0.2c1-0.3,1.9-1,2.5-2c0.6-1,0.8-2.2,0.6-3.2c-0.2-1.1-0.8-2.1-1.7-2.8
+	c-0.3-0.2-0.6-0.4-0.9-0.6l-0.1-0.1l0,0c-1.7-1-3.5-2-5.4-2.8c-1.8-0.8-3.7-1.6-5.7-2.2C65.8,70,62.3,69.2,58.7,68.6z"></path></svg></span>
                     </a>
-                    <a class="u-social-url" target="_blank" data-type="LinkedIn" title="LinkedIn" href=""><span class="u-icon u-social-icon u-social-linkedin u-icon-4"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-0e51"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-0e51"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M41.3,83.7H27.9V43.4h13.4V83.7z M34.6,37.9L34.6,37.9c-4.6,0-7.5-3.1-7.5-7c0-4,3-7,7.6-7s7.4,3,7.5,7
-            C42.2,34.8,39.2,37.9,34.6,37.9z M89.6,83.7H76.2V62.2c0-5.4-1.9-9.1-6.8-9.1c-3.7,0-5.9,2.5-6.9,4.9c-0.4,0.9-0.4,2.1-0.4,3.3v22.5
-            H48.7c0,0,0.2-36.5,0-40.3h13.4v5.7c1.8-2.7,5-6.7,12.1-6.7c8.8,0,15.4,5.8,15.4,18.1V83.7z"></path></svg></span>
-                    </a>
-                    <a class="u-social-url" target="_blank" data-type="Pinterest" title="Pinterest" href=""><span class="u-icon u-social-icon u-social-pinterest u-icon-5"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-7a74"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-7a74"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M61.1,76.9c-4.7-0.3-6.7-2.7-10.3-5c-2,10.7-4.6,20.9-11.9,26.2c-2.2-16.1,3.3-28.2,5.9-41
-            c-4.4-7.5,0.6-22.5,9.9-18.8c11.6,4.6-10,27.8,4.4,30.7C74.2,72,80.3,42.8,71,33.4C57.5,19.6,31.7,33,34.9,52.6
-            c0.8,4.8,5.8,6.2,2,12.9c-8.7-1.9-11.2-8.8-10.9-17.8C26.5,32.8,39.3,22.5,52.2,21c16.3-1.9,31.6,5.9,33.7,21.2
-            C88.2,59.5,78.6,78.2,61.1,76.9z"></path></svg></span>
+                    <a class="u-social-url" target="_blank" data-type="Google Plus" title="Google Plus" href="https://myaccount.google.com/?utm_source=OGB&amp;tab=mk&amp;utm_medium=app"><span class="plus u-icon u-social-google u-social-icon u-icon-4"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-b1ae"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-b1ae"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M60.8,76.3c-6.6,8.6-19,11.1-29,7.7c-10.6-3.5-18.4-14.2-18.2-25.5C13,44.6,25.3,31.8,39.1,31.6
+	c7.1-0.6,14,2.1,19.3,6.6c-2.2,2.4-4.4,4.8-6.9,7.1c-4.8-2.9-10.4-5-15.9-3.1c-8.8,2.6-14.3,13-10.9,21.8
+	c2.7,9.1,13.7,14.1,22.5,10.2c4.5-1.6,7.5-5.7,8.7-10.2c-5.1-0.1-10.2,0-15.5-0.2c0-3,0-6.1,0-9.2c8.6,0,17.1,0,25.7,0
+	C66.7,62.3,65.5,70.4,60.8,76.3z M98.3,62.5c-2.6,0-5.1,0-7.7,0c0,2.6,0,5.1,0,7.7c-2.6,0-5.1,0-7.7,0c0-2.6,0-5.1,0-7.7
+	c-2.6,0-5.1,0-7.7,0c0-2.6,0-5.1,0-7.7c2.6,0,5.1,0,7.7,0c0-2.6,0-5.1,0.1-7.7c2.6,0,5.1,0,7.7,0c0,2.6,0,5.1,0,7.7
+	c2.6,0,5.1,0,7.7,0C98.3,57.3,98.3,59.9,98.3,62.5z"></path></svg></span>
                     </a>
                   </div>
                 </div>
@@ -654,17 +664,7 @@
     <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-d753"><div class="u-clearfix u-sheet u-sheet-1">
         <p class="u-small-text u-text u-text-variant u-text-1">-NEoNX-<br>63118343 Jirawat Chatree
         </p>
-      </div></footer>
-    <section class="u-backlink u-clearfix u-grey-80">
-      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
-        <span>Website Templates</span>
-      </a>
-      <p class="u-text">
-        <span>created with</span>
-      </p>
-      <a class="u-link" href="" target="_blank">
-        <span>Website Builder Software</span>
-      </a>. 
+
     </section>
   </body>
 </html>
